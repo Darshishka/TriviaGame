1166c42316ac53ebfe8932574f8a986f05911795
Darcy Atlas <foreverdiffrent@live.com>
2019-08-16 03:12:32 -0700

    basic html w/ links to other files. question brainstorm on js file

---
+++ assets/javascript/app.js
@@ -0,0 +1,42 @@
+//trivia question ideas
+    //pokemon
+        //regions
+        //number of gyms
+        //who are the elite four
+        //which is not a pokemon type
+    //legend of zelda
+        //name of fairy in link to the past
+        //ganon
+        //goron
+        //kokiri
+        //name of his sword
+        //original console released on
+        //number of zelda games
+    //doctor who
+        //first doctor
+        //first episode
+        //date of reboot
+        //what does tardis stand for
+        //what does the tardis look like
+        //original network released on
+        //what two words followed rose and the doctor
+    //harry potter
+        //voldemorts real name
+        //what certian spells do
+        //name of all the weasles
+        //name of harrys owl
+        //in goblet of fire, who was mad eye moody really
+        //first book release date
+        //what does the pollyjuice potion do
+        //effects of other potions
+        //name of weasleys shop
+        //name of the bank
+        //hagrids first name
+    //star trek
+        //how many itterations of star trek are there
+        //name of the enterprise
+        //enterprise serial number (NCC-1701)
+        //actors names
+        //what itteration was Data in
+        //what itteration was patrick stewart in
+    //
