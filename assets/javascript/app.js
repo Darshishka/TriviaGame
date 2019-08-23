438a47f42556801af2dfc7eba8d2c87fee9e209e
Darcy Atlas <foreverdiffrent@live.com>
2019-08-23 00:42:34 -0700

    questions mostly working. Next, make countdown timer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,12 +1,7 @@
 //trivia question ideas
     //doctor who
         //first doctor
-        //first episode
-        //date of reboot
-        //what does tardis stand for
         //what does the tardis look like
-        //original network released on
-        //what two words followed rose and the doctor
     //harry potter
         //voldemorts real name
         //what certian spells do
@@ -35,60 +30,94 @@
         //when was the first comic published
 
 
-//Q: Which is not a region in pokemon?
-//A: ["Shinnoh", "Kanto", "Unova", "Jinto"]
-//C: jinto 4
+//Doctor Who
 
-//Q: Which is not a Pokemon type?
-//A: ["Dark", "Shadow", "Fairy", "Normal"]
-//C: shadow 2
-
-//Q: What was the name of Link's fairy companion in Link to the Past?
-//A: ["Navi", "Nani", "Nini", "Havi"]
-//C: navi 1
-
-//Q: Who is villan in Legend of Zelda?
-//A: ["Gandalf", "Gandhi", "Ganon", "Gandly"]
-//C: ganon 3
-
-//Q: In Legend of Zelda, who are the Kokiri?
-//A: ["A tribe of forest children", "A tribe of rock people", "A tribe of fish people", "A tribe of people consisting only of women"]
-//C: forest children 1
-
-//Q: In Legend of Zelda, what is the name of Link's iconic weapon?
-//A: ["Master blade", "Master dagger", "Master knife", "Master sword"]
-//C: master sword 4
-
-//Q: What platform was Legend of Zelda origially released on?
-//A: ["GameCube", "Nintendo 64", "NES", "Super Nintendo"]
-//C: NES 3
-
-//Q: 
-//A: 
-//C: 
+    //Q: 
+    //A:
+    //C:
 
 
+//set up question themes
+//set up timer
+//set up correct answer page
+//set up incorrect answer page
+//answer button click only working on second button click
+    //or after changing button to #answers on line 106
+    //correct answer is givien a value of -1
+//after selecting correct answer for 2nd question
+    //also returns response for 3rd question then moves
+    //to 4th question
+    //responce for the 4th question skips to 7th
 
 var questions = [
-    "Test question 1",
-    "Test question 2"
+    "Which is not a region in pokemon?",
+    "Which is not a Pokemon type?",
+    "What was the name of Link's fairy companion in Link to the Past?",
+    "Who is villan in Legend of Zelda?",
+    "In Legend of Zelda, who are the Kokiri?",
+    "In Legend of Zelda, what is the name of Link's iconic weapon?",
+    "What platform was Legend of Zelda origially released on?",
+    "What was the air date of the first episode of Doctor Who?",
+    "In the show, Doctor who, what does does the name TARDIS stand for?",
+    "",
 
 ]
 var answers = [
-    ["test a incc. 1", "test a incc. 2", "correct a 1", "test a incc. 3"],
-    ["correct a 2", "test a incc. 4", "test a incc. 5", "test a incc. 6"]
+    ["Shinnoh", "Kanto", "Unova", "Jinto"],
+    ["Dark", "Shadow", "Fairy", "Normal"],
+    ["Navi", "Nani", "Nini", "Havi"],
+    ["Gandalf", "Gandhi", "Ganon", "Gandly"],
+    ["A tribe of forest children", "A tribe of rock people", "A tribe of fish people", "A tribe of people consisting only of women"],
+    ["Master blade", "Master dagger", "Master knife", "Master sword"],
+    ["GameCube", "Nintendo 64", "NES", "Super Nintendo"],
+    ["November 22nd, 1963", "November 23rd, 1963" , "November 24th, 1963" , "July 18th, 2005"],
+    ["Tornado Advanced Radar Display Information System", "Time And Relative Dimension In Space", "Tethered Arial Release Developed In Style", "Theraputic Aeromatic Dispersal In Sessions"],
+
 ]
-var correctAnswer = [2,0];
+var correctAnswer = [3, 1, 0, 2, 0, 3, 2, 1, 1];
 var btnMade = false;
 
 var btnIndex = ["btn1", "btn2", "btn3", "btn4"]
 var qNum = 0;
-var score = 0;
+var wins = 0;
+var losses = 0;
 
 document.onclick = function gameStart() {
     document.getElementById("startBtn").style.display = "none";
+    
+//set 60 second countdown timer
+    //when time is up nextQuestion() then repeat
 
-    if (btnMade === false) {
+    console.log("qnum " + qNum);
+
+    
+    $("button").click(function() {
+        var btnClickedId = this.id;
+        var userAnswer = btnIndex.indexOf(btnClickedId);
+        console.log(userAnswer);
+        
+        console.log(correctAnswer[qNum]);
+        if (userAnswer === correctAnswer[qNum]) {
+            console.log("corect answer was chosen");
+            wins++;
+            qNum++;
+            console.log(wins);
+            document.getElementById("score").innerHTML = "Score: " + wins;
+        } else {
+            console.log("inccorect answer was chosen");
+            losses++;
+            qNum++;
+        }
+        
+        if (qNum === questions.length) {
+            $("button").hide()
+            endGame();
+        } else if (qNum < questions.length) {
+            //gameQuestions();
+            nextQuestion();
+        }
+    });
+    if (!btnMade) {
         var button1 = document.createElement("button");
         button1.setAttribute("id","btn1");
         var button2 = document.createElement("button");
@@ -107,43 +136,23 @@ document.onclick = function gameStart() {
 }
 
 function gameQuestions() {
-    document.getElementById("questions").innerHTML = questions[qNum];
-    document.getElementById("btn1").innerHTML = answers[qNum][0];
-    document.getElementById("btn2").innerHTML = answers[qNum][1];
-    document.getElementById("btn3").innerHTML = answers[qNum][2];      
-    document.getElementById("btn4").innerHTML = answers[qNum][3];
 
     if (qNum < questions.length) {
-//
-//need to figure out how to end
-//
-
-    //tells me which button is clicked
-    //tests for correct answer
-        $("button").click(function() {
-            var btnClickedId = this.id;
-            var userAnswer = btnIndex.indexOf(btnClickedId);
-            console.log(userAnswer);
-                
-            console.log(correctAnswer[qNum]);
-            if (userAnswer === correctAnswer[qNum]) {
-                console.log("corect answer was chosen");
-                score++;
-                console.log(score);
-                document.getElementById("score").innerHTML = "Score: " + score;
-                nextQuestion();
-            } else {
-                console.log("inccorect answer was chosen");
-                nextQuestion();
-            }
-        });
+        document.getElementById("questions").innerHTML = questions[qNum];
+        document.getElementById("btn1").innerHTML = answers[qNum][0];
+        document.getElementById("btn2").innerHTML = answers[qNum][1];
+        document.getElementById("btn3").innerHTML = answers[qNum][2];      
+        document.getElementById("btn4").innerHTML = answers[qNum][3];
     }
 }
 
+
+
+
 function nextQuestion() {
-    qNum++;
     gameQuestions();
 }
 function endGame() {
-
+    console.log("game end");
+    clearInterval();
 }
